ed31f7c817a63c710ff3db2f71ed6bda1700217f
Hearhee Kang <114882595+heyyree@users.noreply.github.com>
2022-10-20 12:21:24 -0400

    Update music.js

--- music.js
+++ music.js
@@ -25,8 +25,8 @@ loadSong(songs[songIndex])
 // Update song details 
 function loadSong(song) {
     title.innerText = song 
-    audio.src =`src/music/${song}.mp3`
-    cover.src = `src/images/${song}.jpg`
+    audio.src =`music/${song}.mp3`
+    cover.src = `images/${song}.jpg`
 }
 
 
